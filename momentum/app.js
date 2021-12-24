f10337a5d642c35e72fe57313974f2a4facd5ddf
lamPolar <lovast7@gmail.com>
2021-12-24 14:10:49 +0900

    booleans

--- momentum/app.js
+++ momentum/app.js
@@ -1,28 +1,6 @@
-const a = 5;
-const b = 2;
-console.log(5*2);
-console.log(5+2);
-console.log(5/2);
-
-console.log(a*b);
-console.log(a+b);
-console.log(a/b);
-
-console.log(234);
-console.log("234");
-
-let myName = "hees";
-console.log("hello "+ myName);
-myName = "kim";
-console.log("my new name " + myName);
-
-/*        10
-app.js:4 7
-app.js:5 2.5
-app.js:7 10
-app.js:8 7
-app.js:9 2.5
-app.js:11 234
-app.js:12 234
-app.js:15 hello hees
-app.js:17 my new name kim*/
+const amIFat = true;
+let something;
+console.log(amIFat);
+console.log(something);
+something = null;
+console.log(amIFat, something);
